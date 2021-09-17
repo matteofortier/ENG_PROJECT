06542100c666f625c44ae53ef560920c2528f05e
Matteo <matteofortier30@gmail.com>
2021-09-17 10:27:29 -0400

    commit

---
+++ _writeup.md
@@ -0,0 +1,39 @@
+# Game Genres Labeller - Using Deep Learning Techniques
+
+*Matteo Fortier*
+
+## Design
+
+Today, many people dream of becoming a sucessful game streamer, whether it be on twitch or some other game streaming platform. Some want it as their dream job, while some simply enjoy streaming gaming content and entertainment to many people around the world. However, it is harder than ever to become successful as the market becomes more and more saturated. 9 MILLION unique creators streaming each month, 109,400 average concurrent streamers. What if there was a analytics dashboard that could help starting streamers know their platform and their market better, allowing them to get on trends early, and grow faster as a streamer?
+
+## Data
+
+The Twitch API was used for this project. The API allowed us to collect all current streams at any given moment, sorted in views. Each stream would have all the information on it such as the number of  viewers, the game being played, the language, the streamer, the title, the  tags. There was potential to collect a massive amount of data. But for this proof of concept the amount of data collected was limited to the top 1000 streams, every 30 minutes. Additionally the API does not allow for collection of historical data, so the data available at this time is only around 7 days worth. 
+
+## Algorithms
+
+ ***Data Engineering Pipeline***
+
+- Data Collection From Twitch API to MongoDB Atlas:
+  - Twitch API Requests
+  - Automated using APScheduler -> Set to run every half hour interval
+  - Automated scheduler is run on a light google compute unit
+  - Each 30 minute batch is pushed onto MongoDB Atlas
+- Data Collection  From MongoDB Atlas:
+  - Using pymongo requests and read-only user
+- Data Visualisation Using Plotly
+- Web-app using Dash
+  - Deployment using heroku
+
+## Tools
+
+- MongoDB, MongoDB Atlas
+- APScheduler
+- Google Cloud Platform
+- Plotly
+- Dash
+- Heroku
+
+## Communication
+
+The project used powerpoint for the presentation and dash, plotly and heroku for the web-app
--- dashboard-app/app.py
+++ dashboard-app/app.py
@@ -13,6 +13,8 @@ import datetime
 
 app = dash.Dash(__name__)
 
+server = app.server
+
 top_languages = atlas_requests.top_languages_by_views().index
 top_games = atlas_requests.top_games_by_views().index
 
@@ -29,6 +31,7 @@ app.layout = html.Div([
         "Timezone",
          dcc.Dropdown(id="select_timezone",
                      options=[
+                         {"label": 'UTC', "value": 'UTC'},
                          {"label": 'US/Pacific', "value": 'US/Pacific'},
                          {"label": 'US/Central', "value": 'US/Central'},
                          {"label": 'US/Eastern', "value": 'US/Eastern'},
@@ -41,6 +44,35 @@ app.layout = html.Div([
     ]),
    
 
+    html.Br(),
+    html.H2("Viewership Distribution Between Games"),
+    html.Div([
+        "Games",
+         dcc.Dropdown(id="select_games_pie",
+                     options=[
+                         {'label': x, 'value': x} for x in top_games],
+                     multi=True,
+                     value=top_games[:3],
+                     style={'width': "100%"},
+                     clearable=False
+                 ),
+    ]),
+    html.Div([
+        "Increment",
+         dcc.Dropdown(id="select_increment_area",
+                     options=[
+                         {"label": 'Half-Hourly', "value": 'T'},
+                         {"label": 'Hourly', "value": 'H'},
+                         {"label": 'Daily', "value": 'D'}],
+                     multi=False,
+                     value='T',
+                     style={'width': "40%"},
+                     clearable=False
+                 ),
+    ]),
+    dcc.Graph(id='graph_pie', figure={}),
+    dcc.Graph(id='graph_area', figure={}),
+    
     html.Br(),
     html.H2("Viewers Over Time"),
     html.Div([
@@ -66,7 +98,7 @@ app.layout = html.Div([
                      options=[
                          {'label': x, 'value': x} for x in top_languages],
                      multi=True,
-                     value=top_languages[:5],
+                     value=top_languages[:3],
                      style={'width': "40%"},
                      clearable=False
                  ),
@@ -85,12 +117,73 @@ app.layout = html.Div([
                  ),
     ]),
     dcc.Graph(id='graph_votbl', figure={}),
+    
+    html.Br(),
+    html.H2("Viewers Over Time By Game"),
+    html.Div([
+        "Games",
+         dcc.Dropdown(id="select_games_votbg",
+                     options=[
+                         {'label': x, 'value': x} for x in top_games],
+                     multi=True,
+                     value=top_games[:3],
+                     style={'width': "100%"},
+                     clearable=False
+                 ),
+    ]),
+    html.Div([
+        "Increment",
+         dcc.Dropdown(id="select_increment_votbg",
+                     options=[
+                         {"label": 'Half-Hourly', "value": 'T'},
+                         {"label": 'Hourly', "value": 'H'},
+                         {"label": 'Daily', "value": 'D'}],
+                     multi=False,
+                     value='T',
+                     style={'width': "40%"},
+                     clearable=False
+                 ),
+    ]),
+    dcc.Graph(id='graph_votbg', figure={}),
 
 ])
 
 
 # ------------------------------------------------------------------------------
 # Connect the Plotly graphs with Dash Components
+@app.callback(
+    Output(component_id='graph_pie', component_property='figure'),
+    Input(component_id='select_games_pie', component_property='value'),
+)
+def update_votbl(games):
+    df = atlas_requests.top_games_by_views().reset_index()
+    df['game'] = df['game'].apply(lambda x: x if (x in games) else 'Other')
+    fig = px.pie(df, values='num_viewers', names='game', title='Average Distribution Past 7 Days')
+
+    return fig
+
+@app.callback(
+    Output(component_id='graph_area', component_property='figure'),
+    Input(component_id='select_games_pie', component_property='value'),
+    Input(component_id='select_increment_area', component_property='value')
+)
+def update_votbl(games, increment):
+    df = atlas_requests.total_views_per_increment_by_x(increment=increment)
+    df.rename(columns = {'x':'game'}, inplace = True)
+    df['game'] = df['game'].apply(lambda x: x if (x in games) else 'Other')
+    df = df.groupby(['game','date']).sum().reset_index()
+    df2 = atlas_requests.total_views_per_increment(increment=increment).set_index('date')['num_viewers'].to_dict()
+    print(df)
+    df['num_viewers_ratio'] = df.apply(lambda x: x['num_viewers']/df2[x['date']], axis=1)
+    df.reset_index(inplace=True)
+    df['game'] = pd.Categorical(df['game'], ['Other'] + games[::-1] )
+    df.sort_values(['game','date'], inplace = True)
+    fig = px.area(df, x="date", y="num_viewers_ratio", color='game', title='Distribution Over Time')
+
+    return fig
+
+
+
 @app.callback(
      Output(component_id='graph_vot', component_property='figure'),
      Input(component_id='select_timezone', component_property='value'),
@@ -117,6 +210,20 @@ def update_votbl(timezone, increment, languages):
     df['date'] = df['date'].dt.tz_localize('UTC').dt.tz_convert(timezone)
     df.rename(columns = {'x':'language'}, inplace = True)
     fig = px.line(df[df['language'].isin(languages)], x="date", y="num_viewers", color="language", labels={'date': '', 'num_viewers': ''})
+
+    return fig
+
+@app.callback(
+    Output(component_id='graph_votbg', component_property='figure'),
+    Input(component_id='select_timezone', component_property='value'),
+    Input(component_id='select_increment_votbg', component_property='value'),
+    Input(component_id='select_games_votbg', component_property='value'),
+)
+def update_votbl(timezone, increment, games):
+    df = atlas_requests.total_views_per_increment_by_x(increment, x='$game_name')
+    df['date'] = df['date'].dt.tz_localize('UTC').dt.tz_convert(timezone)
+    df.rename(columns = {'x':'game'}, inplace = True)
+    fig = px.line(df[df['game'].isin(games)], x="date", y="num_viewers", color="game", labels={'date': '', 'num_viewers': ''})
 
     return fig
 
